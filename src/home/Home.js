c45cbcad3de85b6d83dd6a5dc62c37ad7ab99333
youness <neopnba@gmail.com>
2020-11-03 18:48:19 +0100

    modify navbar delete from public

--- src/home/Home.js
+++ src/home/Home.js
@@ -12,11 +12,11 @@ export class Home extends Component {
                 <div className="header">
                     <div className="realheader">
                         <Title style={brandStyle} level={2}>Matcha</Title>
-                        <div style={btnStyle}>
-                            <Button ghost shape="round" size={"large"}>
+                        {/* <div style={btnStyle}> */}
+                            <Button ghost style={newstyle} className="newstyle" shape="round">
                                 Login
                             </Button>
-                        </div>
+                        {/* </div> */}
                     </div>
                     <div className="content">
                         <h1 style={{color: '#ffffff', fontSize: '50'}} level={1}>This is New !</h1>
@@ -35,11 +35,21 @@ export class Home extends Component {
 
 const brandStyle = {
     color: '#ffffff',
-    padding: '2% 5%',
+    fontSize: '4vw',
+    margin: '2% 4%'
 }
 
 const btnStyle={
-    padding: '2% 2%'
+    padding: '2% 2%',
+    background: 'red'
 }
 
+const newstyle = {
+    fontSize: '2.5vw',
+    paddingBottom: '2%',
+    height: '5vw',
+    margin: '2% 2%',
+    borderWidth: '1.5px',
+
+}
 export default Home
