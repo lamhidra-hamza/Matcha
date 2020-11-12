732311e119848539712d395bc52a9e118f0d5370
youness <neopnba@gmail.com>
2020-11-12 18:15:20 +0100

    sidebar routing

--- src/components/desktopSection/DesktopSection.js
+++ src/components/desktopSection/DesktopSection.js
@@ -7,23 +7,34 @@ import ChatBox from '../chatBox/ChatBox'
 import UserInfo from '../userInfo/UserInfo'
 import EditProfile from '../editProfile/EditProfile'
 import './DesktopSection.css'
+import {Switch, Route, useRouteMatch, Redirect} from 'react-router-dom'
 
 function DesktopSection(props) {
     const [showProfile, setShowProfile] = useState(false);
     const { width } = props;
+    let match = useRouteMatch();
 
     return (
         <div className="mainRow">
             <div className="leftSide">
             <NavbarApp setShowProfile={setShowProfile} showProfile={showProfile}/>
-            {showProfile ? <ProfileInfo/> : <Maintab/>}
+            {showProfile ? 
+                    <><ProfileInfo/>
+                    <Redirect to={`${match.url}/profile`}/></>
+                :
+                    <Maintab/>
+                }
             </div>
-            <EditProfile />
-            {/* <Infocard/>
-            <div className="rightSide">
-            <ChatBox />
-            {width > 1300 ? <UserInfo /> : null}
-            </div> */}
+            <Switch>
+                <Route exact path={`${match.url}/profile`} component={EditProfile}/>
+                <Route exact path={`${match.url}`} component={Infocard}/>
+                <Route path={`${match.url}/chatbox`}>
+                    <div className="rightSide">
+                    <ChatBox />
+                    {width > 1300 ? <UserInfo /> : null}
+                    </div>
+                </Route>
+            </Switch>
         </div>
     )
 }
--- src/components/mainTab/Maintab.js
+++ src/components/mainTab/Maintab.js
@@ -4,26 +4,21 @@ import './Maintab.css'
 import MatcheDisplay from '../matchesDisplay/MatcheDisplay'
 import MessageDisplay from '../messageDisplay/MessageDisplay';
 import { Badge } from 'antd';
-import { Redirect } from 'react-router-dom';
+import { Redirect,  Link } from 'react-router-dom';
 
 
 const { TabPane } = Tabs;
 
-export default function Maintab({match}) {
-    const tabClick = (key) => {
-        if (key === '2') {
-            console.log(match.url);
-            <Redirect to={`${match.url}/messages`}/>
-        }
-    }
-
+export default function Maintab() {
     return (
         <div className="tabs">
-            <Tabs className="tabsstyle" defaultActiveKey="1" style={{fontWeight: '800'}} onTabClick={tabClick}>
-                <TabPane style={{ height: '100vh', width: '400px'}} tab={<span>Matches <Badge count={25} /></span>} key="1">
+            <Tabs className="tabsstyle" defaultActiveKey="1" style={{fontWeight: '800'}}>
+                <TabPane style={{ height: '100vh', width: '400px'}} tab={
+                    <Link to="/app/desktop/"><span>Matches <Badge count={25} /></span> </Link> } key="1">
                     <MatcheDisplay/>
                 </TabPane>
-                <TabPane style={{ height: '100vh', width: '400px'}}  tab={<span>Messagees <Badge count={2} /></span>} key="2">
+                <TabPane style={{ height: '100vh', width: '400px'}}  tab={
+                <Link to="/app/desktop/chatbox"> <span>Messagees <Badge count={2} /></span> </Link> } key="2">
                     <MessageDisplay />
                 </TabPane>
             </Tabs>
