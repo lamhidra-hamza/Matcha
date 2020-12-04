3a5627e0f555bf45115d792576e9f02abc35e6fc
ztatbat <zakariae.tatbat@gmail.com>
2020-12-04 09:35:06 +0100

    add LoginForm

---
+++ src/components/loginForm/LoginForm.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react'
+import './LoginForm.css'
+import { Modal, Button } from 'antd'
+import { Form, Input, Checkbox } from 'antd'
+
+
+export class LoginForm extends Component {
+  state = {
+    loading: false,
+    visible: false,
+    brandStyle: {},
+    componentSize: 'default',
+  }
+
+  getStyle = () => {
+    return {
+      textAlign: 'center',
+      margin: '2% 4%',
+      borderRadius: '50px',
+      height: this.props.mobile ? '100vh' : '500px',
+    }
+  }
+
+  handleOk = () => {
+    this.setState({ loading: true })
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false })
+    }, 3000)
+  }
+
+  onFinish = (values) => {
+    console.log('Success:', values)
+  }
+
+  onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
+  }
+
+  render() {
+    const { loading } = this.state
+
+    return (
+      <div>
+        <Modal
+          visible={this.props.visible}
+          onOk={this.handleOk}
+          onCancel={this.props.handleCancel}
+          bodyStyle={this.getStyle()}
+          width={this.props.mobile ? '100vw' : '50vw'}
+          centered={true}
+          footer={[
+            <Button key="back" onClick={this.props.handleCancel}></Button>,
+            <Button
+              key="submit"
+              type="primary"
+              loading={loading}
+              onClick={this.handleOk}
+            ></Button>,
+          ]}
+        >
+          <div className="loginoption">
+            <Form
+              {...layout}
+              name="basic"
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={this.onFinish}
+              onFinishFailed={this.onFinishFailed}
+            >
+              <Form.Item
+                label="Email"
+                name="Email"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item
+                {...tailLayout}
+                name="remember"
+                valuePropName="checked"
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+
+export default LoginForm
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react'
-import Header from '../../components/header/Header'
-import Content from '../../components/content/Content'
+import React, { Component } from 'react';
+import Header from '../../components/header/Header';
+import Content from '../../components/content/Content';
 
-import Footer from '../../components/footer/Footer'
-import './Home.css'
-import Loginpopup from '../../components/loginpopup/Loginpopup'
-//import Registerpopup from '../../components/registerpopup/Registerpopup'
-import RegisterForm from '../../components/registerForm/RegisterForm'
+import Footer from '../../components/footer/Footer';
+import './Home.css';
+import Loginpopup from '../../components/loginpopup/Loginpopup';
+import Registerpopup from '../../components/registerpopup/Registerpopup';
+import RegisterForm from '../../components/registerForm/RegisterForm';
+import LoginForm from '../../components/loginForm/LoginForm';
 
 export class Home extends Component {
   state = {
     loginVisible: false,
-    registerVisible: false
+    registerVisible: false,
+    loginFormVisible: true,
   }
   showLogin = () => {
     this.setState({
-       loginVisible: !this.state.loginVisible,
+       loginVisible: !this.state.loginVisible
+    });
+  }
+
+  showLoginForm = () => {
+    this.setState({
+      loginFormVisible : !this.state.loginFormVisible
     });
   }
 
@@ -37,8 +45,9 @@ export class Home extends Component {
           <Content showModal={this.showRegister} mobile={this.props.mobile}/>
           <div className = "center">
             <Loginpopup visible={this.state.loginVisible} handleCancel= {this.handleCancel} mobile={this.props.mobile}/>
-            {/* <Registerpopup visible={this.state.registerVisible} handleCancel= {this.handleCancel} mobile={this.props.mobile}/> */}
-            <RegisterForm visible={this.state.registerVisible} handleCancel= {this.handleCancel} mobile={this.props.mobile}/>
+            <Registerpopup visible={this.state.registerVisible} handleCancel= {this.handleCancel} mobile={this.props.mobile}/>
+            {/* <RegisterForm visible={this.state.registerVisible} handleCancel= {this.handleCancel} mobile={this.props.mobile}/> */}
+            <LoginForm visible={this.state.loginFormVisible} handleCancel= {this.handleCancel} mobile={this.props.mobile} />
           </div>
           <Footer />
         </div>
