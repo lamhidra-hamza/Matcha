6b604fbd58ed453ea58ad6ae45f6db7977c935ff
ztatbat <zakariae.tatbat@gmail.com>
2021-01-22 12:35:48 +0100

    form input validation

--- src/components/registerForm/RegisterForm.js
+++ src/components/registerForm/RegisterForm.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import './RegisterForm.scss'
-import axios from 'axios'
+import React from "react";
+import "./RegisterForm.scss";
+import axios from "axios";
 import {
   Modal,
   Form,
   Input,
   Tooltip,
   Checkbox,
-  Button, DatePicker
-} from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
-import { SER } from '../../conf/config';
-
+  Button,
+  DatePicker,
+} from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import { SER } from "../../conf/config";
+import moment from "moment";
 
 const formItemLayout = {
   labelCol: {
@@ -36,177 +37,232 @@ const tailFormItemLayout = {
   },
 };
 
-const RegisterForm = (props) => {  
+const RegisterForm = (props) => {
   const getStyle = () => {
     return {
-      textAlign: 'center',
-      margin: '2% 4%',
-      borderRadius: '50px',
-      height: props.mobile ? '100vh' : '500px',
-    }
-  }
+      textAlign: "center",
+      margin: "2% 4%",
+      borderRadius: "50px",
+      height: props.mobile ? "100vh" : "500px",
+    };
+  };
 
   const [form] = Form.useForm();
 
   const onFinish = async (values) => {
-	props.showModal(values.email);
-	console.log(values)
-	const result = await axios.post(`${SER.HOST}/api/users/signup`, values);
-	await axios.post(`${SER.HOST}/api/pictures`, 
-			{
-				user_id: result.data.id,
-				picture_1: null,
-				picture_2: null,
-				picture_3: null,
-				picture_4: null,
-				picture_5: null
-			});
-	await axios.post(`${SER.HOST}/api/location`, {
-		user_id: result.data.id,
-        longitude: null,
-    	latitude: null,
-	})
- };
+    props.showModal(values.email);
+    const result = await axios.post(`${SER.HOST}/api/users/signup`, values);
+    await axios.post(`${SER.HOST}/api/pictures`, {
+      user_id: result.data.id,
+      picture_1: null,
+      picture_2: null,
+      picture_3: null,
+      picture_4: null,
+      picture_5: null,
+    });
+    await axios.post(`${SER.HOST}/api/location`, {
+      user_id: result.data.id,
+      longitude: null,
+      latitude: null,
+    });
+  };
+
+  const checkAge = (value) => {
+    console.log(moment().year() - moment(value).year());
+    if (moment().year() - moment(value).year() > 18) return true;
+    return false;
+  };
 
   return (
     <div className="registerForm">
       <Modal
-          visible={props.visible}
-          onOk={"this.handleOk"}
-          onCancel={props.handleCancel}
-          bodyStyle={getStyle()} 
-          width={props.mobile ? '100vw' : '50vw'}
-          centered={true}
-          footer = {[
-            <Button key="back" onClick={props.handleCancel}></Button>,
-          ]}
-        >
-          <div className="loginoption">
-			<Form
-				{...formItemLayout}
-				form={form}
-				name="register"
-				onFinish={onFinish}
-				initialValues={{
-					residence: ['zhejiang', 'hangzhou', 'xihu'],
-					prefix: '86',
-				}}
-        scrollToFirstError
-				>
-				<Form.Item
-					name="email"
-					label="E-mail"
-					rules={[
-					{
-						type: 'email',
-						message: 'The input is not valid E-mail!',
-					},
-					{
-						required: true,
-						message: 'Please input your E-mail!',
-					},
-					]}
-				>
-					<Input />
-				</Form.Item>
-				<Form.Item
-					name="username"
-					label={
-					<span>
-						Username&nbsp;
-						<Tooltip title="What do you want others to call you?">
-						<QuestionCircleOutlined />
-						</Tooltip>
-					</span>
-					}
-					rules={[{ required: true, message: 'Please input your Usename!', whitespace: true }]}
-				>
-					<Input />
-				</Form.Item>
-				<Form.Item
-					name="firstName"
-					label="First Name"
-					rules={[{ required: true, message: 'Please input your First Name!', whitespace: true }]} 
-					>
-					<Input />
-				</Form.Item>
-				<Form.Item
-					name="lastName"
-					label="Last Name"
-					rules={[{ required: true,
-						message: 'Please input your Last Name!',
-						 whitespace: true }]} 
-					>
-					<Input />
-				</Form.Item>
-				<Form.Item
-					name="bornDate"
-					label="Born Date"
-					>
-					<DatePicker style={{width: '100%'}} />
-				</Form.Item>
-				<Form.Item style={{display: "flex", flexWrap: "wrap",justifyContent: "space-between"}}
-					name="password"
-					label="Password"
-					rules={[
-					{
-						required: true,
-						message: 'Please input your password!',
-					},
-					]}
-					hasFeedback
-				>
-					<Input.Password />
-				</Form.Item>
+        visible={props.visible}
+        onOk={"this.handleOk"}
+        onCancel={props.handleCancel}
+        bodyStyle={getStyle()}
+        width={props.mobile ? "100vw" : "50vw"}
+        centered={true}
+        footer={[<Button key="back" onClick={props.handleCancel}></Button>]}
+      >
+        <div className="loginoption">
+          <Form
+            {...formItemLayout}
+            form={form}
+            name="register"
+            onFinish={onFinish}
+            initialValues={{
+              residence: ["zhejiang", "hangzhou", "xihu"],
+              prefix: "86",
+            }}
+            scrollToFirstError
+          >
+            <Form.Item
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your E-mail!",
+                },
+                {
+                  pattern: /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/,
+                  max: 250,
+                  message: "please enter a valid Email",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="username"
+              label={
+                <span>
+                  Username&nbsp;
+                  <Tooltip title="What do you want others to call you?">
+                    <QuestionCircleOutlined />
+                  </Tooltip>
+                </span>
+              }
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+                {
+                  pattern: /^[a-zA-Z0-9]+$/,
+                  max: 100,
+                  min: 3,
+                  message: "please enter a valid Username",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="firstName"
+              label="First Name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your First Name!",
+                },
+                {
+                  pattern: /^(?=.{1,50}$)[a-z]+(?:['_.\s][a-z]+)*$/i,
+                  message: "please enter a valid Name",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="lastName"
+              label="Last Name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Last Name!",
+                },
+                {
+                  pattern: /^(?=.{1,50}$)[a-z]+(?:['_.\s][a-z]+)*$/i,
+                  message: "please enter a valid Name",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="bornDate"
+              label="Born Date"
+              rules={[
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || checkAge(value)) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      "You should have 18 years Old to sign-up"
+                    );
+                  },
+                }),
+              ]}
+            >
+              <DatePicker style={{ width: "100%" }} />
+            </Form.Item>
+            <Form.Item
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+              }}
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+                {
+                  pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                  message: "Minimum eight characters, at least one letter and one number",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password />
+            </Form.Item>
 
-				<Form.Item
-					name="confirm"
-					label="confirm Password"
-					dependencies={['password']}
-					hasFeedback
-					rules={[
-					{
-						required: true,
-						message: 'Please confirm your password!',
-					},
-					({ getFieldValue }) => ({
-						validator(rule, value) {
-						if (!value || getFieldValue('password') === value) {
-							return Promise.resolve();
-						}
-						return Promise.reject('The two passwords that you entered do not match!');
-						},
-					}),
-					]}
-				>
-					<Input.Password />
-				</Form.Item>
+            <Form.Item
+              name="confirm"
+              label="confirm Password"
+              dependencies={["password"]}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: "Please confirm your password!",
+                },
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      "The two passwords that you entered do not match!"
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-				<Form.Item
-					name="agreement"
-					valuePropName="checked"
-					rules={[
-					{
-						validator: (_, value) =>
-						value ? Promise.resolve() : Promise.reject('Should accept agreement'),
-					},
-					]}
-					{...tailFormItemLayout}
-				>
-					<Checkbox>
-					I have read the <a href="#">agreement</a>
-					</Checkbox>
-				</Form.Item>
-				<Form.Item {...tailFormItemLayout}>
-					<Button type="primary" htmlType="submit">
-						Register
-					</Button>
-				</Form.Item>
-				</Form>
-			</div>
-        </Modal>
+            <Form.Item
+              name="agreement"
+              valuePropName="checked"
+              rules={[
+                {
+                  validator: (_, value) =>
+                    value
+                      ? Promise.resolve()
+                      : Promise.reject("Should accept agreement"),
+                },
+              ]}
+              {...tailFormItemLayout}
+            >
+              <Checkbox>
+                I have read the <a href="#">agreement</a>
+              </Checkbox>
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Modal>
     </div>
   );
-}
+};
 
-export default RegisterForm
+export default RegisterForm;
